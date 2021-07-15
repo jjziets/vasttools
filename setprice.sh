06a8f69733b85b529d596d04656c2f202e9051c8
Etherion <19214485+jjziets@users.noreply.github.com>
2021-07-15 15:25:07 +0200

    Update setprice.sh

--- setprice.sh
+++ setprice.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 #this script sets the pricing of listed host on vast to x1.8 mining of etherum
+gpuhash=120
+margin=1.75
 
 get_json() {
     local response=$(curl --silent -H "accept: application/json" "$1" -w "\t\t%{http_code}")
@@ -23,29 +25,27 @@ do
 now=$(date +"%T")
 echo "Update pricing : $now"
 
-rev=$(get_json "https://whattomine.com/coins/151.json?hr=120&p=420.0&fee=0.0&cost=0.1&hcost=0.0&span_br=1h&span_d=24&commit=Calculate" | jq .revenue )
+rev=$(get_json "https://whattomine.com/coins/151.json?hr=$gpuhash&p=420.0&fee=0.0&cost=0.1&hcost=0.0&span_br=1h&span_d=24&commit=Calculate" | jq .revenue )
 
 if [ $? -eq 0 ]
-then 
+then
 
         rev=$(echo $rev | sed  's/\$//g')
         rev=$(echo $rev | sed  's/"//g')
-        rev=$(bc <<< "scale=2; $rev/24*2")
+        rev=$(bc <<< "scale=2; $rev/24*$margin")
         echo "New Price $rev"
 
-        ./vast list machine 3451 --price_gpu $rev --price_disk 2 --price_inetu 0.02 --price_inetd 0.02 --min_chunk 1
+#        ./vast list machine 3451 --price_gpu $rev --price_disk 1 --price_inetu 0.02 --price_inetd 0.02 --min_chunk 6
+#        sleep 1
 
-        sleep 1 
-
-        ./vast list machine 2787 --price_gpu $rev --price_disk 2 --price_inetu 0.02 --price_inetd 0.02 --min_chunk 4
+        ./vast list machine 2787 --price_gpu $rev --price_disk 0.5 --price_inetu 0.02 --price_inetd 0.02 --min_chunk 4
 
         sleep 1
 
-        rev=$(bc <<< "scale=2; $rev*0.9")
+#        rev=$(bc <<< "scale=2; $rev*1.1")
 
-        ./vast list machine 3129 --price_gpu $rev --price_disk 2 --price_inetu 0.02 --price_inetd 0.02 --min_chunk 4
+        ./vast list machine 3129 --price_gpu $rev --price_disk 1 --price_inetu 0.02 --price_inetd 0.02 --min_chunk 6
         sleep 600 # sleep for 10 min
 fi
 sleep 1
 done
-
