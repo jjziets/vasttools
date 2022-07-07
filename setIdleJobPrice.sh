5f117d039489d731cfef807e5c61ddf693943ebb
Etherion <19214485+jjziets@users.noreply.github.com>
2022-07-07 15:58:11 +0200

    Update setIdleJobPrice.sh

--- setIdleJobPrice.sh
+++ setIdleJobPrice.sh
@@ -1,15 +1,10 @@
-
-#!/bin/bash
-
-Instances=($(./vast show instances  | sed 's/|/ /'  | awk '{print $1}' )) # get all the instanses number from vast
-
-unset Instances[0] #delte the first index as it containe ID
-
-for i in "${Instances[@]}"; do
-    echo "$i"
-        ./vast change bid "$i" --price 0.3 #set the price for each
-done
-
-
-
-
+# Parse and Execute Arguments passed to script                           {{{1
+ if (( $(echo "$1 > 0" |bc -l) )); then
+        Instances=($(./vast show instances  | sed 's/|/ /'  | awk '{print $1}' )) # get all the instanses number from vast
+        unset Instances[0] #delte the first index as it containe ID
+        for i in "${Instances[@]}"; do
+                ./vast change bid "$i" --price $1 #set the price for each
+        done
+  else
+  echo "usage sudo  ./setIdleJobPrice.sh 0.3"
+ fi
