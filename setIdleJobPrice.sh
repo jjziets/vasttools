7e29ea24f63a8362201b1c8df42ab7170646c54d
Etherion <19214485+jjziets@users.noreply.github.com>
2022-07-08 15:48:31 +0200

    Update setIdleJobPrice.sh

--- setIdleJobPrice.sh
+++ setIdleJobPrice.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#to be used with vastcli in a shell. It will set the price of all the instnases listed in your account to the arg past. 0.3 is $0.3/h/instance. In this case as there is only one gpu per instces.
 # Parse and Execute Arguments passed to script                           {{{1
  if (( $(echo "$1 > 0" |bc -l) )); then
         Instances=($(./vast show instances  | sed 's/|/ /'  | awk '{print $1}' )) # get all the instanses number from vast
