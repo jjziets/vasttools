28155e7de0754403a7bbfaa4c7a052a1b4ef6417
Etherion <19214485+jjziets@users.noreply.github.com>
2023-03-19 02:48:14 +0200

    Update speedtest-cli.py

--- speedtest-cli.py
+++ speedtest-cli.py
@@ -4,8 +4,8 @@
 import subprocess
 import json
 import datetime
-from geopy.geocoders import Nominatim
-import pycountry
+#from geopy.geocoders import Nominatim
+#import pycountry
 
 max_retries = 3
 retry_count = 0
@@ -13,7 +13,7 @@ retry_count = 0
 while retry_count < max_retries:
     try:
         # Run the speedtest command and capture the output
-        output = subprocess.check_output(["speedtest", "--format=json"])
+        output = subprocess.check_output(["speedtest", "--accept-license","--format=json"])
         break
     except subprocess.CalledProcessError as e:
         print(f"Error running speedtest: {e}")
@@ -26,25 +26,25 @@ while retry_count < max_retries:
 data = json.loads(output)
 
 # Initialize geolocator
-geolocator = Nominatim(user_agent="my_app")
+#geolocator = Nominatim(user_agent="my_app")
 
 # Enter the city and country name
 city = data["server"]["location"]
 country = data["server"]["country"]
 
 # Combine the city and country name into a single address
-location = geolocator.geocode(f"{city}, {country}")
+#location = geolocator.geocode(f"{city}, {country}")
 
-longitude  = round(float(location.longitude),4)
-latitude   = round(float(location.latitude),4)
+#longitude  = round(float(location.longitude),4)
+#latitude   = round(float(location.latitude),4)
 
 
-def country_to_code(country_name):
-    try:
-        country_code = pycountry.countries.search_fuzzy(country_name)[0].alpha_2
-    except LookupError:
-        country_code = None
-    return country_code
+#def country_to_code(country_name):
+#    try:
+#        country_code = pycountry.countries.search_fuzzy(country_name)[0].alpha_2
+#    except LookupError:
+#        country_code = None
+#    return country_code
 
 
 
@@ -57,10 +57,10 @@ result = {
         "ispdlavg": "0",
         "ip": data["interface"]["externalIp"],
         "isp": data["isp"],
-        "lon": str(longitude),
+        "lon": "0", #str(longitude),
         "ispulavg": "0",
-        "country": country_to_code(data["server"]["country"]),
-        "lat": str(latitude)
+        "country": " ", #country_to_code(data["server"]["country"]),
+        "lat": "0", # str(latitude)
     },
     "bytes_sent": data["upload"]["bytes"],
     "download": float(str(data["download"]["bandwidth"]) + '.123300') * 8,
@@ -74,13 +74,13 @@ result = {
         "name": data["server"]["location"],
         "url": "http://{}:{}/upload.php".format(data['server']['host'], data['server']['port']),
         "country": data["server"]["country"],
-        "lon":  str(longitude), #"0",  #data["server"]["lon"],
-        "cc": country_to_code(data["server"]["country"]),
+        "lon":  "0", # str(longitude), #"0",  #data["server"]["lon"],
+        "cc": "  ", #country_to_code(data["server"]["country"]),
         "host": "{}:{}".format(data['server']['host'], data['server']['port']),
         "sponsor": data["server"]["name"],
-        "lat": str(latitude), # "0", # data["server"]["lat"],
-        "id": str(data["server"]["id"]),
-        "d": data["ping"]["latency"]/1000.0
+        "lat": "0", #str(latitude), # "0", # data["server"]["lat"],
+        "id": "0", #str(data["server"]["id"]),
+        "d": data["ping"]["latency"]
     }
 }
 
